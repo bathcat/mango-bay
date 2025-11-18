de09f7d7536d3906ccaa1a1961f416cef2012923
bathcat <eric.mcmullen@gmail.com>
2025-11-18 06:54:31 -0700

    Fix merge.

--- projects/web/src/app/shared/ui/rich-text-display.component.ts
+++ projects/web/src/app/shared/ui/rich-text-display.component.ts
@@ -1,14 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'mbc-rich-text-display',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div [innerHTML]="content()"></div>
+    <div [innerHTML]="trustedHtml()"></div>
   `,
 })
 export class RichTextDisplayComponent {
   content = input<string>('');
+
+  trustedHtml = computed<SafeHtml>(() => {
+    return this.sanitizer.bypassSecurityTrustHtml(this.content());
+  });
+
+  constructor(private sanitizer: DomSanitizer) {}
 }
